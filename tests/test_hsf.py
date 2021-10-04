916ae903ba0ad48d548fa7e53c17093629a53965
Clément POIRET <poiret.clement@outlook.fr>
2021-10-04 20:58:16 +0200

    [TESTS] Segmentation test

--- tests/test_hsf.py
+++ tests/test_hsf.py
@@ -1,9 +1,12 @@
 import shutil
 import tempfile
+from pathlib import Path
 
 import ants
 import hsf.fetch_models
 import hsf.roiloc_wrapper
+import hsf.segment
+import torch
 from hsf import __version__
 
 tmpdir = tempfile.mkdtemp()
@@ -32,7 +35,7 @@ def test_roiloc():
 
     _, right, left = hsf.roiloc_wrapper.get_hippocampi(mri, {
         "contrast": "t2",
-        "margin": [16, 8, 16],
+        "margin": [2, 0, 2],
         "roi": "hippocampus"
     }, mask)
 
@@ -41,6 +44,17 @@ def test_roiloc():
 
     # Saving to {tmpdir}/tse_{side}_hippocampus.nii.gz
     hsf.roiloc_wrapper.save_hippocampi(right, left, tmpdir, mris[0])
+
+
+def test_segment():
+    mri = Path(tmpdir + "/tse_right_hippocampus.nii.gz")
+    sub = hsf.segment.mri_to_subject(mri)
+    session = hsf.segment.get_inference_sessions(
+        tmpdir, providers=["CPUExecutionProvider"])[0]
+
+    pred = hsf.segment.predict(sub, session, "1/2/3")
+
+    hsf.segment.save_prediction(mri, pred[0])
 
 
 shutil.rmtree(tmpdir)

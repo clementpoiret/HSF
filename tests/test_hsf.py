55fc60d8cf9145a637426e5613d8776d9f6abea9
Clément POIRET <poiret.clement@outlook.fr>
2021-10-05 10:53:10 +0200

    [TESTS] Using pytest fixtures with session scope

--- tests/test_hsf.py
+++ tests/test_hsf.py
@@ -1,14 +1,9 @@
-import shutil
-import tempfile
-from pathlib import Path
-
 import ants
 import hsf.fetch_models
 import hsf.roiloc_wrapper
 import hsf.segment
+import pytest
 from hsf import __version__
-
-tmpdir = tempfile.mkdtemp()
 
 
 def test_version():
@@ -16,17 +11,19 @@ def test_version():
 
 
 # Models getters
-def test_fetching():
+@pytest.fixture(scope="session")
+def test_fetching(tmp_path):
     """Tests that we can download and hash a model."""
     url = "https://zenodo.org/record/5524594/files/arunet_bag0.onnx?download=1"
     xxh3 = "d0de65baa81d9382"
 
-    hsf.fetch_models.fetch(tmpdir, "model.onnx", url, xxh3)
+    hsf.fetch_models.fetch(tmp_path, "model.onnx", url, xxh3)
 
-    assert xxh3 == hsf.fetch_models.get_hash(tmpdir + "/model.onnx")
+    assert xxh3 == hsf.fetch_models.get_hash(tmp_path / "model.onnx")
 
 
-def test_roiloc():
+@pytest.fixture(scope="session")
+def test_roiloc(tmp_path):
     """Tests that we can locate and save hippocampi."""
     mris = hsf.roiloc_wrapper.load_from_config("tests/mri", "*.nii.gz")
     assert mris
@@ -44,19 +41,17 @@ def test_roiloc():
     assert isinstance(left, ants.ANTsImage)
 
     # Saving to {tmpdir}/tse_{side}_hippocampus.nii.gz
-    hsf.roiloc_wrapper.save_hippocampi(right, left, tmpdir, mris[0])
+    hsf.roiloc_wrapper.save_hippocampi(right, left, tmp_path, mris[0])
 
 
-def test_segment():
+@pytest.fixture(scope="session")
+def test_segment(tmp_path):
     """Tests that we can segment and save a hippocampus."""
-    mri = Path(tmpdir + "/tse_right_hippocampus.nii.gz")
+    mri = tmp_path / "tse_right_hippocampus.nii.gz"
     sub = hsf.segment.mri_to_subject(mri)
     session = hsf.segment.get_inference_sessions(
-        tmpdir, providers=["CPUExecutionProvider"])[0]
+        tmp_path, providers=["CPUExecutionProvider"])[0]
 
     pred = hsf.segment.predict(sub, session, "1/2/3")
 
     hsf.segment.save_prediction(mri, pred[0])
-
-
-shutil.rmtree(tmpdir)

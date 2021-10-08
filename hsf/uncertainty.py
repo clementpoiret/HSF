103bf150826dc9aabf7595d39448a43eec2b3041
Clément POIRET <poiret.clement@outlook.fr>
2021-10-08 15:18:52 +0200

    [CODE] Added voxel-wise uncertainty

--- hsf/uncertainty.py
+++ hsf/uncertainty.py
@@ -3,19 +3,27 @@
 # https://raw.githubusercontent.com/ivadomed/ivadomed/ac29c7cbe299e6478854823ed6bd818c0e5f9642/ivadomed/uncertainty.py
 # Copyright (c) 2019 Polytechnique Montreal, Université de Montréal
 import ants
+import torch
 
 
-def voxelwise_uncertainty(samples: list) -> ants.ANTsImage:
+def voxelwise_uncertainty(samples_prob: torch.Tensor,
+                          eps: float = 1e-5) -> torch.Tensor:
     """
     Computes the voxelwise uncertainty of a list of samples as entropy.
 
     Args:
-        samples (list): List of predictions.
+        samples_prob (torch.Tensor): Softmaxed predictions.
+        eps (float): Epsilon factor to avoid log(0).
 
     Returns:
-        ants.ANTsImage: The voxelwise uncertainty map.
+        torch.Tensor: The voxelwise uncertainty map.
     """
-    raise NotImplementedError("Voxelwise Uncertainty not yet implemented.")
+    unc = samples_prob.permute(0, 2, 3, 4, 1)
+    unc = -torch.sum(
+        torch.mean(unc, 0) * torch.log(torch.mean(unc, 0) + eps), -1)
+    unc[unc < 0] = 0
+
+    return unc
 
 
 def structural_uncertainty(samples: list) -> ants.ANTsImage:

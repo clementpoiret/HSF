5e33ce0bb1b5a931797547b6cb77a9d19d6789a9
Clément POIRET <poiret.clement@outlook.fr>
2021-10-11 15:38:54 +0200

    [CODE] Uncertainty computations

--- hsf/factory.py
+++ hsf/factory.py
@@ -2,15 +2,18 @@ from pathlib import Path, PosixPath
 
 import ants
 import hydra
+import torch
 # from hydra import compose, initialize
 from omegaconf import DictConfig
 from rich import print as pprint
+from roiloc.locator import RoiLocator
 
 from hsf.fetch_models import fetch_models
 from hsf.roiloc_wrapper import (get_hippocampi, get_mri, load_from_config,
                                 save_hippocampi)
 from hsf.segment import (get_inference_sessions, mri_to_subject,
                          save_prediction, segment)
+from hsf.uncertainty import voxelwise_uncertainty
 from hsf.welcome import welcome
 
 # initialize(config_path="hsf/conf")
@@ -19,6 +22,16 @@ PREFIX = "[italic white]FACTORY >"
 
 
 def get_lr_hippocampi(mri: PosixPath, cfg: DictConfig) -> tuple:
+    """
+    Get left and right hippocampi from a given MRI.
+
+    Args:
+        mri (PosixPath): Path to the MRI.
+        cfg (DictConfig): Configuration.
+
+    Returns:
+        tuple: Tuple containing locator, orientation, left and right hippocampi.
+    """
     image, bet_mask = get_mri(mri, cfg.files.mask_pattern)
     pprint(image)
     original_orientation = image.orientation
@@ -40,6 +53,79 @@ def get_lr_hippocampi(mri: PosixPath, cfg: DictConfig) -> tuple:
         left_mri=left_mri,
         dir_name=cfg.files.output_dir,
         original_mri_path=mri)
+
+
+def predict(mri: PosixPath, sessions: list, cfg: DictConfig) -> tuple:
+    """
+    Predict the hippocampal segmentation for a given MRI.
+
+    Args:
+        mri (PosixPath): Path to the MRI.
+        sessions (list): List of ONNX Runtime sessions.
+        cfg (DictConfig): Configuration.
+
+    Returns:
+        tuple: Tuple containing soft and hard segmentations.
+    """
+    subject = mri_to_subject(mri)
+
+    pprint(f"{PREFIX} Starting segmentation...")
+    return segment(
+        subject=subject,
+        augmentation_cfg=cfg.augmentation,
+        segmentation_cfg=cfg.segmentation.segmentation,
+        sessions=sessions,
+        ca_mode=str(cfg.segmentation.ca_mode),
+    )
+
+
+def compute_uncertainty(mri: PosixPath, soft_pred: torch.Tensor) -> None:
+    """
+    Compute uncertainty for a given set of segmentations.
+
+    Args:
+        mri (PosixPath): Path to the MRI.
+        soft_pred (torch.Tensor): Soft segmentations.
+    """
+    uncertainty = voxelwise_uncertainty(soft_pred)
+    pprint(f"{PREFIX} Saving voxel-wise uncertainty map in {str(mri.parent)}")
+    _ = save_prediction(mri=mri,
+                        prediction=uncertainty,
+                        suffix="unc_crop",
+                        astype="float64")
+
+
+def save(mri: PosixPath, hippocampus: PosixPath, hard_pred: torch.Tensor,
+         locator: RoiLocator, orientation: str) -> None:
+    """
+    Save segmentations.
+
+    Args:
+        mri (PosixPath): Path to the MRI.
+        hippocampus (PosixPath): Path to the hippocampus.
+        hard_pred (torch.Tensor): Hard segmentations.
+        locator (RoiLocator): RoiLocator.
+        orientation (str): Orientation of the original MRI.
+    """
+    pprint(f"{PREFIX} Saving cropped segmentation in LPI orientation.")
+    segmentation = save_prediction(mri=hippocampus,
+                                   prediction=hard_pred,
+                                   suffix="seg_crop")
+
+    native_segmentation = locator.inverse_transform(segmentation)
+
+    if orientation != "LPI":
+        pprint(f"{PREFIX} Reorienting segmentation to original orientation...")
+        native_segmentation = ants.reorient_image2(native_segmentation,
+                                                   orientation=orientation)
+
+    extensions = "".join(mri.suffixes)
+    fname = hippocampus.name.replace(extensions, "") + "_seg.nii.gz"
+    output_path = mri.parent / fname
+
+    ants.image_write(native_segmentation, str(output_path))
+
+    pprint(f"{PREFIX} Saved segmentation in native space to {str(output_path)}")
 
 
 @hydra.main(config_path="conf", config_name="config")
@@ -70,42 +156,15 @@ def main(cfg: DictConfig) -> None:
         locator, orientation, hippocampi = get_lr_hippocampi(mri, cfg)
 
         for j, hippocampus in enumerate(hippocampi):
-            pprint(f"{PREFIX} Subject {i}/{N}, side {j}/1")
             hippocampus = Path(hippocampus)
-            subject = mri_to_subject(hippocampus)
 
-            pprint(f"{PREFIX} Starting segmentation...")
-            _, prediction = segment(
-                subject=subject,
-                augmentation_cfg=cfg.augmentation,
-                segmentation_cfg=cfg.segmentation.segmentation,
-                sessions=sessions,
-                ca_mode=str(cfg.segmentation.ca_mode),
-            )
+            pprint(f"{PREFIX} Subject {i+1}/{N}, side {j+1}/2")
+            soft_pred, hard_pred = predict(hippocampus, sessions, cfg)
 
-            pprint(f"{PREFIX} Saving cropped segmentation in LPI orientation.")
-            segmentation = save_prediction(mri=hippocampus,
-                                           prediction=prediction,
-                                           suffix="seg_crop")
+            if soft_pred.shape[0] > 1:
+                compute_uncertainty(hippocampus, soft_pred)
 
-            native_segmentation = locator.inverse_transform(segmentation)
-
-            if orientation != "LPI":
-                pprint(
-                    f"{PREFIX} Reorienting segmentation to original orientation..."
-                )
-                native_segmentation = ants.reorient_image2(
-                    native_segmentation, orientation=orientation)
-
-            extensions = "".join(hippocampus.suffixes)
-            fname = hippocampus.name.replace(extensions, "") + "_seg.nii.gz"
-            output_path = mri.parent / fname
-
-            ants.image_write(native_segmentation, str(output_path))
-
-            pprint(
-                f"{PREFIX} Saved segmentation in native space to {str(output_path)}"
-            )
+            save(mri, hippocampus, hard_pred, locator, orientation)
 
 
 def start():
--- hsf/uncertainty.py
+++ hsf/uncertainty.py
@@ -2,7 +2,6 @@
 # and is borrowed from the ivadomed repository:
 # https://raw.githubusercontent.com/ivadomed/ivadomed/ac29c7cbe299e6478854823ed6bd818c0e5f9642/ivadomed/uncertainty.py
 # Copyright (c) 2019 Polytechnique Montreal, Université de Montréal
-import ants
 import torch
 
 
@@ -12,28 +11,18 @@ def voxelwise_uncertainty(samples_prob: torch.Tensor,
     Computes the voxelwise uncertainty of a list of samples as entropy.
 
     Args:
-        samples_prob (torch.Tensor): Softmaxed predictions.
+        samples_prob (torch.Tensor): Softmaxed predictions in NCHWD format.
         eps (float): Epsilon factor to avoid log(0).
 
     Returns:
         torch.Tensor: The voxelwise uncertainty map.
     """
+    if samples_prob.shape[1] == 1:
+        samples_prob = torch.cat((samples_prob, 1 - samples_prob), dim=1)
+
     unc = samples_prob.permute(0, 2, 3, 4, 1)
     unc = -torch.sum(
         torch.mean(unc, 0) * torch.log(torch.mean(unc, 0) + eps), -1)
     unc[unc < 0] = 0
 
     return unc
-
-
-def structural_uncertainty(samples: list) -> ants.ANTsImage:
-    """
-    Compute the structural uncertainty of a list of samples.
-
-    Args:
-        samples (list): List of predictions.
-
-    Returns:
-        ants.ANTsImage: Average voxel-wise uncertainty within the structure.
-    """
-    raise NotImplementedError("Structural Uncertainty not yet implemented.")

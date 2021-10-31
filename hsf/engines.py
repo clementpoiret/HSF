fc318462c24a1ad69d1f24710ff0fc2a3bb4c54b
Clément POIRET <poiret.clement@outlook.fr>
2021-10-31 15:02:01 +0100

    [CODE] Created InferenceEngine class

--- hsf/engines.py
+++ hsf/engines.py
@@ -1,7 +1,30 @@
 from pathlib import Path, PosixPath
 
+import deepsparse
 import onnxruntime as ort
+from omegaconf.dictconfig import DictConfig
 from omegaconf.listconfig import ListConfig
+
+
+def deepsparse_support() -> str:
+    """
+    Check if the CPU supports DeepSparse's optimizations.
+
+    Returns:
+        str: Support Status
+    """
+    avx2 = deepsparse.cpu.cpu_avx2_compatible()
+    avx512 = deepsparse.cpu.cpu_avx512_compatible()
+    vnni = deepsparse.cpu.cpu_vnni_compatible()
+
+    if vnni:
+        return "full"
+    elif avx512:
+        return "partial"
+    elif avx2:
+        return "minimal"
+    else:
+        return "not supported"
 
 
 def get_inference_sessions(models_path: PosixPath, providers: list) -> list:
@@ -34,3 +57,49 @@ def get_inference_sessions(models_path: PosixPath, providers: list) -> list:
         ort.InferenceSession(str(model_path), providers=providers)
         for model_path in models
     ]
+
+
+class InferenceEngine:
+    """
+    Base class for inference engines.
+    """
+
+    def __init__(self, engine: str, engine_settings: DictConfig,
+                 models_path: PosixPath):
+        self.engine = engine
+        self.engine_settings = engine_settings
+
+        if engine == "deepsparse":
+            support = deepsparse_support()
+            print(f"DeepSparse's optimizations support: {support}")
+
+        p = Path(models_path).expanduser()
+        self.models = list(p.glob("*.onnx"))
+
+    def __call__(self):
+        raise NotImplementedError
+
+    def get_ort_engines(self):
+
+        def _correct_provider(provider):
+            if isinstance(provider, str):
+                return provider
+            elif isinstance(provider, ListConfig):
+                provider = list(provider)
+                assert len(provider) == 2
+                provider[1] = dict(provider[1])
+
+                return tuple(provider)
+
+        providers = [
+            _correct_provider(provider)
+            for provider in self.engine_settings.execution_providers
+        ]
+
+        return [
+            ort.InferenceSession(str(model_path), providers=providers)
+            for model_path in self.models
+        ]
+
+    def get_deepsparse_engines(self):
+        raise NotImplementedError

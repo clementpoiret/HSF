68f8120ae1677caebe954b8227ddcc4e32bb0067
Clément POIRET <poiret.clement@outlook.fr>
2021-11-07 11:30:15 +0100

    [CODE] Fixed none type

--- hsf/welcome.py
+++ hsf/welcome.py
@@ -6,10 +6,8 @@ from rich import print as pprint
 
 from hsf import __version__
 
-URL_PATTERN = 'https://pypi.python.org/pypi/HSF/json'
 
-
-def get_version(url_pattern=URL_PATTERN):
+def get_version(url_pattern='https://pypi.python.org/pypi/HSF/json'):
     """Returns version of HSF on pypi.python.org using json."""
     req = requests.get(url_pattern)
     version = parse('0')
@@ -51,7 +49,8 @@ __/\\\________/\\\_____/\\\\\\\\\\\____/\\\\\\\\\\\\\\\_
 
         """)
 
-    if parse(__version__) < get_version():
+    if parse(__version__) < get_version(
+            'https://pypi.python.org/pypi/HSF/json'):
         pprint(f"""
         A new version of HSF is available (v{str(get_version())}).
         You can update it by running:
